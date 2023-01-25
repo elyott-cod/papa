1dc1fa5aea33e5c0e453d50e56ad278972b7e0e0
elyott-cod <123592046+elyott-cod@users.noreply.github.com>
2023-01-25 20:44:06 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,37 @@
+music.playMelody("C5 - A - F - D C ", 500)
+basic.showLeds(`
+    # # . . .
+    # . # . .
+    # # . . .
+    # . . . .
+    # . . . .
+    `)
+music.playMelody("C5 - A - F - D C ", 500)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+music.playMelody("C5 - A - F - D C ", 500)
+basic.showLeds(`
+    # # . . .
+    # . # . .
+    # # . . .
+    # . . . .
+    # . . . .
+    `)
+music.playMelody("C5 - A - F - D C ", 500)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+basic.showString("ANIVERSERE")
+music.playMelody("C5 - A - F - D C ", 500)
+basic.forever(function () {
+    music.playMelody("C5 - A - F - D C ", 500)
+})
